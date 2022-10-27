29b426086d97bc607a7c985da47bff2a95b3fa4c
Danu Atmaja <danuatmaja@gmail.com>
2022-10-27 22:18:38 +0800

    type assertion -part2

--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,10 +1,18 @@
-// const btn = document.getElementById("btn")!; //put exclamation(!) at the end, means that we make sure that this would not null
+const btn = document.getElementById("btn")! as HTMLButtonElement; //put exclamation(!) at the end, means that we make sure that this would not null
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+
+(<HTMLInputElement>input).value // is not work on jsx/tsx
 
 // btn.addEventListener("click", function() {
 //   alert("CLICKED!");
 // });
 
-let mystery: unknown = "Hello World!!!";
+// let mystery: unknown = "Hello World!!!";
 
-// const numbChars = mystery.length; // this will showing the error because the type is unknown
-const numChars = (mystery as string).length;
+// // const numbChars = mystery.length; // this will showing the error because the type is unknown
+// const numChars = (mystery as string).length;
+
+btn.addEventListener("click", function() {
+  alert(input.value);
+  input.value = "";
+});
