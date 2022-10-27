5b562ec727f00d60e8f7530b43baf8212384b491
Danu Atmaja <danuatmaja@gmail.com>
2022-10-27 22:28:45 +0800

    working on event

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("Hello");
+const btn = document.getElementById("btn"); //put exclamation(!) at the end, means that we make sure that this would not null
+const input = document.getElementById("todoinput");
+const form = document.querySelector("form");
+// (<HTMLInputElement>input).value // is not work on jsx/tsx
+// // btn.addEventListener("click", function() {
+// //   alert("CLICKED!");
+// // });
+// // let mystery: unknown = "Hello World!!!";
+// // // const numbChars = mystery.length; // this will showing the error because the type is unknown
+// // const numChars = (mystery as string).length;
+// btn.addEventListener("click", function() {
+//   alert(input.value);
+//   input.value = "";
+// });
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log("SUBMITTED");
+}
+form.addEventListener("submit", handleSubmit);
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,18 +1,26 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement; //put exclamation(!) at the end, means that we make sure that this would not null
 const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
 
-(<HTMLInputElement>input).value // is not work on jsx/tsx
+// (<HTMLInputElement>input).value // is not work on jsx/tsx
+
+// // btn.addEventListener("click", function() {
+// //   alert("CLICKED!");
+// // });
+
+// // let mystery: unknown = "Hello World!!!";
+
+// // // const numbChars = mystery.length; // this will showing the error because the type is unknown
+// // const numChars = (mystery as string).length;
 
 // btn.addEventListener("click", function() {
-//   alert("CLICKED!");
+//   alert(input.value);
+//   input.value = "";
 // });
 
-// let mystery: unknown = "Hello World!!!";
+function handleSubmit (e: SubmitEvent) {
+  e.preventDefault();
+  console.log("SUBMITTED");
+}
 
-// // const numbChars = mystery.length; // this will showing the error because the type is unknown
-// const numChars = (mystery as string).length;
-
-btn.addEventListener("click", function() {
-  alert(input.value);
-  input.value = "";
-});
+form.addEventListener("submit", handleSubmit);
