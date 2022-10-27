1ee73e15ee3ecfe49c32c1fd47aa0d8f9fe584d5
Danu Atmaja <danuatmaja@gmail.com>
2022-10-27 21:45:00 +0800

    type assertion

--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,6 +1,10 @@
-const btn = document.getElementById("btn")!; //put exclamation(!) at the end, means that we make sure that this would not null
+// const btn = document.getElementById("btn")!; //put exclamation(!) at the end, means that we make sure that this would not null
 
-btn.addEventListener("click", function() {
-  alert("CLICKED!");
-});
+// btn.addEventListener("click", function() {
+//   alert("CLICKED!");
+// });
 
+let mystery: unknown = "Hello World!!!";
+
+// const numbChars = mystery.length; // this will showing the error because the type is unknown
+const numChars = (mystery as string).length;
