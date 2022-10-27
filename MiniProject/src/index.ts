bc8197f39f5f568b456ecfc7f88189520ec7de49
Danu Atmaja <danuatmaja@gmail.com>
2022-10-27 21:40:51 +0800

    non null assertion

--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,1 +1,6 @@
-console.log(document.getElementById("btn"));
+const btn = document.getElementById("btn")!; //put exclamation(!) at the end, means that we make sure that this would not null
+
+btn.addEventListener("click", function() {
+  alert("CLICKED!");
+});
+
